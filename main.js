3000e6ca0d4f951c7c6ebc8c628b12d39551f942
bigguysahaj <singhsahaj2001@gmail.com>
2022-09-17 17:49:24 +0530

    added functionality

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const canvas = document.getElementById("myCanvas");
-canvas.height=window.innerHeight;
-canvas.width= 200;
+canvas.height = window.innerHeight;
+canvas.width = 200;
 
-const ctx = canvas
+const ctx = canvas.getContext("2d");
+const car = new Car(100,100,30,50);
+car.draw(ctx);
+
+animate();
+
+function animate(){
+    car.update();
+    canvas.height = window.innerHeight;//this is to make the canvas resize with the window (very important)
+    //without this method, the car will just draw a big black trail behind it.
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
