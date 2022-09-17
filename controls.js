092a85fa258fddb8336898df98b285fe6039ff38
bigguysahaj <singhsahaj2001@gmail.com>
2022-09-17 17:48:47 +0530

    created controls

---
+++ controls.js
@@ -0,0 +1,47 @@
+class Controls{
+    constructor(){
+        this.forward = false;
+        this.left = false;
+        this.right = false;
+        this.reverse = false;
+
+        this.#addKeyboardListerners();
+    }
+    #addKeyboardListerners(){
+        //this is for listens to the arrow key inputs
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowUp":
+                    this.forward = true;
+                    break;
+                case "ArrowLeft":
+                    this.left = true;
+                    break;
+                case "ArrowRight":
+                    this.right=true;
+                    break;
+                case "ArrowDown":
+                    this.reverse = true;
+                    break;
+            }
+            console.table(this);
+        }
+        document.onkeyup = (event)=>{
+            switch(event.key){
+                case "ArrowUp":
+                    this.forward = false;
+                    break;
+                case "ArrowLeft":
+                    this.left = false;
+                    break;
+                case "ArrowRight":
+                    this.right = false;
+                    break;
+                case "ArrowDown":
+                    this.reverse = false;
+                    break;
+            }
+            console.table(this);
+        }
+    }
+}
