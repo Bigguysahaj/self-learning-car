4368488cd386d6b87e72b76cae9637c618fc8105
bigguysahaj <singhsahaj2001@gmail.com>
2022-09-26 17:44:02 +0530

    Car is functional now

--- controls.js
+++ controls.js
@@ -24,7 +24,6 @@ class Controls{
                     this.reverse = true;
                     break;
             }
-            console.table(this);
         }
         document.onkeyup = (event)=>{
             switch(event.key){
@@ -41,7 +40,7 @@ class Controls{
                     this.reverse = false;
                     break;
             }
-            console.table(this);
+
         }
     }
 }
