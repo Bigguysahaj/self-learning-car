17fc602e76f9024b8d0b01e3ab6d04c0954ef9cc
bigguysahaj <singhsahaj2001@gmail.com>
2022-10-04 17:07:35 +0530

    added roadmarkers

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ canvas.height = window.innerHeight;
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width/2,canvas.width*0.7);
+const road = new Road(canvas.width/2,canvas.width*0.9);
 const car = new Car(100,100,30,50);
 
 
--- road.js
+++ road.js
@@ -1,5 +1,5 @@
 class Road{
-    constructor(x,width,laneCount=3){
+    constructor(x,width,laneCount=4){
 
         // drawing road
         this.x=x;
@@ -21,14 +21,22 @@ class Road{
         ctx.lineWidth=5;
         ctx.strokeStyle='yellow';
 
-        ctx.beginPath();
-        ctx.moveTo(this.left,this.top);
-        ctx.lineTo(this.left,this.bottom);
-        ctx.stroke();
 
-        ctx.beginPath();
-        ctx.moveTo(this.right,this.top);
-        ctx.lineTo(this.right,this.bottom);
-        ctx.stroke();
+        for(let i =0; i<=this.laneCount; i++){
+            const x = lerp(
+                this.left,
+                this.right,
+                i/this.laneCount
+            );
+            if(i>0 && i<this.laneCount){
+                ctx.setLineDash([20,20]);
+            }else{
+                ctx.setLineDash([]);
+            }
+            ctx.beginPath();
+            ctx.moveTo(x,this.top);
+            ctx.lineTo(x,this.bottom);
+            ctx.stroke();
+        }
     }
 }
