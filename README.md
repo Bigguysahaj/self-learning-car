430e9876fe8731b7f51bb3c2a5d7fd2c27caaeff
bigguysahaj <singhsahaj2001@gmail.com>
2022-10-07 01:39:50 +0530

    The camera moves with the car now

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # self-learning-car
+
+## Current update
+
+<!-- [picture] -->
+
+Whats New: (7th october) 
+* The camera now follows the car in a bird eye view perspective.
+* The car trully seems to being moving because of the animated roads.
+* We only used save(), restore() and translate() function to do this.
+
+
--- main.js
+++ main.js
@@ -14,7 +14,14 @@ function animate(){
 
     canvas.height = window.innerHeight;//this is to make the canvas resize with the window (very important)
     //without this method, the car will just draw a big black trail behind it.
+    ctx.save();
+    ctx.translate(0,-car.y+canvas.height*0.6);
     road.draw(ctx); //draw the road, here the road comes first, before the car
     car.draw(ctx);
+
+    // camera following car thing, is so easy, just needed the above
+    // ctx.translate(0,-car.y+canvas.height*0.6); and ctx.save()
+    // and ctx.restore() at the end of the function
+    ctx.restore();
     requestAnimationFrame(animate);
 }
